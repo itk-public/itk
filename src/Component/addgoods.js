8f8d1618b2ecb8cd9362a16a8c3f9a4ffbea3276
叶分洲 <1150691947@qq.com>
2017-04-29 14:19:34 +0800

    first

--- src/Component/addgoods.js
+++ src/Component/addgoods.js
@@ -1,9 +1,107 @@
 import React, { Component } from 'react';
+import { FormGroup, Checkbox, Radio, ControlLabel, FormControl, Button } from "react-bootstrap";
 import {Link} from 'react-router';
 import Lists from './Lists';
 import Paginate from './Paginate';
 import Dropzone from "react-dropzone";
+import "../Stylesheets/addgoods.css";
+function FieldGroup({ id, label, help, ...props }) {
+  return (
+    <FormGroup controlId={id}>
+      <ControlLabel>{label}</ControlLabel>
+      <FormControl {...props} />
+      {help && <HelpBlock>{help}</HelpBlock>}
+    </FormGroup>
+  );
+}
 
+const formInstance = (
+  <form>
+    <FieldGroup
+      id="formControlsText"
+      type="text"
+      label="Text"
+      placeholder="Enter text"
+    />
+    <FieldGroup
+      id="formControlsEmail"
+      type="email"
+      label="Email address"
+      placeholder="Enter email"
+    />
+    <FieldGroup
+      id="formControlsPassword"
+      label="Password"
+      type="password"
+    />
+  
+
+    <Checkbox checked readOnly>
+      Checkbox
+    </Checkbox>
+    <Radio checked readOnly>
+      Radio
+    </Radio>
+
+    <FormGroup>
+      <Checkbox inline>
+        1
+      </Checkbox>
+      {' '}
+      <Checkbox inline>
+        2
+      </Checkbox>
+      {' '}
+      <Checkbox inline>
+        3
+      </Checkbox>
+    </FormGroup>
+    <FormGroup>
+      <Radio name="radioGroup" inline>
+        1
+      </Radio>
+      {' '}
+      <Radio name="radioGroup" inline>
+        2
+      </Radio>
+      {' '}
+      <Radio name="radioGroup" inline>
+        3
+      </Radio>
+    </FormGroup>
+
+    <FormGroup controlId="formControlsSelect">
+      <ControlLabel>Select</ControlLabel>
+      <FormControl componentClass="select" placeholder="select">
+        <option value="select">select</option>
+        <option value="other">...</option>
+      </FormControl>
+    </FormGroup>
+    <FormGroup controlId="formControlsSelectMultiple">
+      <ControlLabel>Multiple select</ControlLabel>
+      <FormControl componentClass="select" multiple>
+        <option value="select">select (multiple)</option>
+        <option value="other">...</option>
+      </FormControl>
+    </FormGroup>
+
+    <FormGroup controlId="formControlsTextarea">
+      <ControlLabel>Textarea</ControlLabel>
+      <FormControl componentClass="textarea" placeholder="textarea" />
+    </FormGroup>
+
+    <FormGroup>
+      <ControlLabel>Static text</ControlLabel>
+      <FormControl.Static>
+        email@example.com
+      </FormControl.Static>
+    </FormGroup>
+
+    <Button type="submit">
+      Submit
+    </Button>
+  </form>
+);
 
 export default class Addgoods extends Component {
   constructor(props) {
@@ -26,10 +124,11 @@ class Dropzon extends Component{
 
     render () { 
       return (
-          <div>
+          <div className="add-goods">
             <Dropzone onDrop={this.onDrop}>
               <div>Try dropping some files here, or click to select files to upload.</div>
             </Dropzone>
+            { formInstance }
           </div>
       );
     }
